812a60e65bb52372abe82431402cda0ed5b43df3
Thiago Ramon Gonçalves Montoya <thiagoramon@gmail.com>
2013-03-18 00:13:59 -0300

    Adding initial release

---
+++ DeadlyReentry/DeadlyReentry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DeadlyReentry
+{
+    public class DeadlyReentry : KSP.Testing.UnitTest
+    {
+        public DeadlyReentry()
+        {
+            GameObject ghost = new GameObject("DeadlyReentryGhost", typeof(DeadlyReentryGhost));
+            GameObject.DontDestroyOnLoad(ghost);
+
+            base.TestStartUp();
+        }
+    }
+
+    public class DeadlyReentryGhost : MonoBehaviour
+    {
+        AerodynamicsFX afx;
+
+        void Update()
+        {
+            if (FlightGlobals.ready && (FlightGlobals.ActiveVessel != null))
+            {
+                if (afx == null)
+                {
+                    GameObject fx = GameObject.Find("FXLogic");
+                    if (fx != null)
+                    {
+                        afx = fx.GetComponent<AerodynamicsFX>();
+                    }
+                }
+
+                if (afx != null)
+                {
+                    if (afx.FxScalar > 0)
+                    {
+                        //print("DeadlyReentryGhost - FxScalar = " + afx.FxScalar);
+                        foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+                        {
+                            p.temperature += afx.FxScalar * 1000.0f * TimeWarp.deltaTime;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
